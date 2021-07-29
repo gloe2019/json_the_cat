7ac964054e8ccb3c8beb6142d22ff0210fcce5ce
Gloria Edem <gloedem@gmail.com>
2021-07-29 17:44:13 +0000

    refactored code: userfacing logic moved to index.js, fnc returns desc/err via callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,24 @@
 const request = require("request");
-const args = process.argv.slice(2);
-// console.log(typeof args);
-let query = args[0];
-// console.log(query);
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${query}`,
-  (error, response, body) => {
-    // console.log("response: ", response.statusCode);
-    // console.log("body: ", body);
-    if (error) {
-      console.log("❌Request Failed❌");
-      console.log(error);
-      return;
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        // console.log("Request Failed");
+        callback(error, null);
+      } else {
+        const data = JSON.parse(body);
+        if (data[0] === undefined) {
+          let error = "Error => breed name not found";
+          callback(error, null);
+        } else {
+          // const data = JSON.parse(body);
+          callback(null, data[0].description);
+        }
+      }
     }
-    console.log("response: ", response.statusCode);
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log("❌Error => breed name not found❌");
-      return "Error - requested breed not found!";
-    }
-    console.log("breed name: ", data[0].name);
-    console.log("description: ", data[0].description);
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
